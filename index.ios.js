2f73f738c8345abf4399b72bce451faf995ba276
shz8 <administrator@zhanghongtaodeMacBook-Pro.local>
2016-10-29 21:19:49 +0800

    Revert "1"
    This reverts commit 349e691c58757d41574c51b70cf8292a4bff2073.

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class zsapp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!1234
+          Welcome to React Native!123
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
